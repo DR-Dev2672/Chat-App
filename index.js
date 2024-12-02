e7d89aee4c003d462dc555e6cfb7730b8b20c0a3
dev <thakurdev2672@gmail.com>
2024-12-02 23:47:46 +0530

    styling added

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const {Server} =require("socket.io")
 
 
 const io=new Server(server)
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 01fbe9b8edd5851c57d8ef6d1fa8e0f933d41d54
 io.on("connection",(socket)=>{
     socket.on("user-message",(message)=>{
       io.emit("message",message)
